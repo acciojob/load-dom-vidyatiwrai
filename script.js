eb0527b43c98869d181037a902ce0c08c82fcfdc
Vidya Tiwari <127404171+vidyatiwrai@users.noreply.github.com>
2023-06-16 09:24:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+//your JS code here. If required.
+    document.addEventListener('DOMContentLoaded', function() {
+      var body = document.querySelector('body');
+      body.textContent = 'DOM load success';
+    });
